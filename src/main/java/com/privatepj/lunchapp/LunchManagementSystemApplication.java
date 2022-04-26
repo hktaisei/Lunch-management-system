81299fb8e2c4a2a9c34dd685977a3e7eb6c7ded3
hktaisei <mtsaihokutann17@outlook.jp>
2022-04-26 20:50:14 +0900

    First commit

---
+++ src/main/java/com/privatepj/lunchapp/LunchManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.privatepj.lunchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/privatepj/lunchapp/LunchManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.privatepj.lunchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
