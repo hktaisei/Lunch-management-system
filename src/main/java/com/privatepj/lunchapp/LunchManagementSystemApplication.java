91a015a019e3cb46de72b348c071502244c574b6
hktaisei <mtsaihokutann17@outlook.jp>
2022-05-02 06:55:20 +0900

    微修正

--- src/main/java/com/privatepj/lunchapp/LunchManagementSystemApplication.java
+++ src/main/java/com/privatepj/lunchapp/LunchManagementSystemApplication.java
@@ -8,7 +8,6 @@ public class LunchManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LunchManagementSystemApplication.class, args);
-		//dev_main
 	}
 
 }
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>aaaInsert title here</title>
+</head>
+<body>
+
+    jsp
+
+
+</body>
+</html>
